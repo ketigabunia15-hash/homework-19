891991f8c48fcbdc726f2a683b3c0487dfb330ec
ketigabunia15-hash <ketigabunia15@gmail.com>
2025-11-24 18:26:32 +0400

    img tag

---
+++ main.js
@@ -0,0 +1,9 @@
+const img = document.createElement("img");
+
+img.src = "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
+
+img.style.width = "400px";
+img.style.display = "block";
+img.style.margin = "20px auto";
+
+document.body.appendChild(img);
